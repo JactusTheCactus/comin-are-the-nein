24c50bb248974ed2dfa16e1dfded5b910fa7bf36
JactusTheCactus <threateninglyamusing@gmail.com>
2026-02-18 06:35:21 -0800

    Update .sh

--- .sh
+++ .sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 flag() {
-	for f in "$@"; do
-		[[ -e ".flags/$f" ]] || return 1
+	for f in "$@"
+		do [[ -e ".flags/$f" ]] || return 1
 	done
 }
 npm run sync
 npm run page
-if flag local; then
-	npm run watch
-else
-	npm run build
+if flag local
+	then npm run watch
+	else npm run build
 fi
 npm run post
